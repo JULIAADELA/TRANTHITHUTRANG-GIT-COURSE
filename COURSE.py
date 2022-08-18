57ac9946bf1b34d6e6f5a1664e32d1ad45642bfe
JULIAADELA <110718513+JULIAADELA@users.noreply.github.com>
2022-08-18 23:25:25 +0700

    Add files via upload

---
+++ COURSE.py
@@ -0,0 +1,22 @@
+# Problem 1:
+def addition(a, b):
+    return a + b
+
+print(addition(7,9))
+
+# Problem 2:
+def subtract(a, b):
+    return a - b
+
+print(subtract(9,3))
+
+# Problem 3:
+#You are now still in the main/master branch, go to your Terminal, create a new branch name: :"add-divide-feature"
+# Switch/Checkout to that branch, and do task 4 and 5.
+
+# problem 4:
+def divide(a, b):
+    return a/b
+print(divide(20,2))
+
+# Problem 5:
